cb8311bc7d4adc3d339d6af43175d375518f377c
Madhusoodan P <akshayapataki123@gmail.com>
2021-05-02 10:30:40 +0530

    Added a primitive markdown editor

--- src/main/resources/static/posts-plugin/posts.js
+++ src/main/resources/static/posts-plugin/posts.js
@@ -56,6 +56,7 @@ class posts {
                             {
                                 ele: 'input',
                                 classList: 'title',
+                                iden: 'title',
                                 attribs: {
                                     value: obj ? obj.title : ""
                                 },
@@ -68,12 +69,105 @@ class posts {
                             {
                                 ele: 'span',
                                 attribs: {
-                                    innerHTML: `Content (<a href='https://guides.github.com/features/mastering-markdown/'>Markdown</a> + HTML supported)`
+                                    innerHTML: `Content (<a href='https://guides.github.com/features/mastering-markdown/' target='_blank' title='Markdown tutorial'>Markdown</a> + HTML supported)`
                                 }
+                            },
+                            {
+                                ele: 'div',
+                                classList: 'editor-controls',
+                                children: [
+                                    {
+                                        ele: 'select',
+                                        attribs: { style: 'vertical-align: text-bottom;' },
+                                        children: [
+                                            { ele: 'option', text: 'Header 1' },
+                                            { ele: 'option', text: 'Header 2' },
+                                            { ele: 'option', text: 'Header 3' },
+                                            { ele: 'option', text: 'Header 4' }
+                                        ],
+                                        evnts: {
+                                            change: function () {
+                                                let V = { "Header 1": '#', "Header 2": '##', "Header 3": '###', "Header 4": '####' }
+                                                self.surround(V[this.value] + " ", " ")
+                                            }
+                                        }
+                                    },
+                                    {
+                                        ele: 'b', text: 'B', classList: 'ctrl txt-ctrl ctrl-bold', attribs: { src: '/posts-plugin/bold.png' },
+                                        evnts: { click: function (e) { self.surround("__") } }
+                                    },
+                                    {
+                                        ele: 'i', text: 'I', classList: 'ctrl txt-ctrl ctrl-italic', attribs: { src: '/posts-plugin/bold.png' },
+                                        evnts: { click: function (e) { self.surround("_") } }
+                                    },
+                                    {
+                                        ele: 'u', text: 'U', classList: 'ctrl txt-ctrl ctrl-underline', attribs: { src: '/posts-plugin/bold.png' },
+                                        evnts: { click: function (e) { self.surround("<u>", "</u>") } }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/image.png', title: 'Insert images' },
+                                        evnts: {
+                                            click: function () {
+                                                filePicker([], 'Pick an image')
+                                                    .then(fileNames => {
+                                                        self.insertImgLinks(fileNames)
+                                                        self.showPreview()
+                                                    })
+                                            }
+                                        }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/attach24.png', title: 'Attach files' },
+                                        evnts: {
+                                            click: function () {
+                                                filePicker([], 'Pick a file')
+                                                    .then(fileNames => {
+                                                        self.attachments = [...new Set(self.attachments.concat(fileNames))]
+                                                        self.updateAttachmentView()
+                                                    })
+                                            }
+                                        }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/code.png', title: 'Insert code snippet' },
+                                        evnts: { click: function (e) { self.surround("```\n", "\n```") } }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/quote.png', title: 'Insert quote' },
+                                        evnts: { click: function (e) { self.surround("\n> ", " ") } }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/bullet.png', title: 'List' },
+                                        evnts: { click: function (e) { self.listview("- ") } }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/numbered-bullet.png', title: 'List' },
+                                        evnts: { click: function (e) { self.listview("1. ") } }
+                                    },
+                                    {
+                                        ele: 'img',
+                                        classList: 'ctrl img-ctrl',
+                                        attribs: { src: '/posts-plugin/table.png', title: 'List' },
+                                        evnts: { click: function (e) { self.insertAtCursor(self.editor, "Col1 | Col2\n-------|--------\n1 | 2\n3 | 4"); self.showPreview() } }
+                                    },
+                                ]
                             },
                             {
                                 ele: "textarea",
                                 classList: "editor",
+                                iden: 'editor',
                                 attribs: {
                                     rows: 20,
                                     value: obj ? obj.content : ""
@@ -91,76 +185,107 @@ class posts {
                             {
                                 ele: "div",
                                 classList: "preview",
-                                evnts: {}
+                                iden: 'preview'
                             },
                             {
                                 ele: 'div',
-                                classList: 'attach-actions',
-                                children: [
-                                    {
-                                        ele: 'button',
-                                        text: 'Attach files',
-                                        evnts: {
-                                            click: function () {
-                                                filePicker(self.attachments, 'Pick a file')
-                                                    .then(fileNames => {
-                                                        self.attachments = fileNames
-                                                        self.showPreview()
-                                                    })
-                                            }
-                                        }
-                                    },
-                                    {
-                                        ele: 'button',
-                                        text: 'Add image',
-                                        evnts: {
-                                            click: function () {
-                                                filePicker(self.attachments, 'Pick an image')
-                                                    .then(fileNames => {
-                                                        self.insertImgLinks(fileNames)
-                                                        self.showPreview()
-                                                    })
-                                            }
-                                        }
-                                    }
-                                ]
+                                iden: 'attachmentsView',
+                                classList: 'attachments-view'
                             }
                         ]
                     }
                 ]
             }
         }
-        let ele = render('posts', renderable(obj));
-        this.title = ele.getElementsByTagName('input')[0];
-        this.editor = ele.getElementsByTagName('textarea')[0];
-        this.preview = ele.getElementsByClassName('posts-preview')[0];
+        let ele = render('posts', renderable(obj), (id, ele) => self[id] = ele);
         return ele;
     }
 
-    insertImgLinks(urls) {
-        function insertAtCursor(myField, myValue) {
-            //IE support
-            if (document.selection) {
-                myField.focus();
-                sel = document.selection.createRange();
-                sel.text = myValue;
-            }
-            //MOZILLA and others
-            else if (myField.selectionStart || myField.selectionStart == '0') {
-                var startPos = myField.selectionStart;
-                var endPos = myField.selectionEnd;
-                myField.value = myField.value.substring(0, startPos)
-                    + myValue
-                    + myField.value.substring(endPos, myField.value.length);
-            } else {
-                myField.value += myValue;
+    listview(typ) {
+        var sel = this.getInputSelection(this.editor), val = this.editor.value;
+        if (sel.start == sel.end) return
+        this.editor.value = val.slice(0, sel.start) + "\n" + typ + val.slice(sel.start, sel.end).replaceAll(/(\n[\t ]*)/gi, "$1" + typ) + "\n" + val.slice(sel.end);
+        this.showPreview()
+    }
+
+    getInputSelection(el) {
+        var start = 0, end = 0, normalizedValue, range,
+            textInputRange, len, endRange;
+
+        if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+            start = el.selectionStart;
+            end = el.selectionEnd;
+        } else {
+            range = document.selection.createRange();
+
+            if (range && range.parentElement() == el) {
+                len = el.value.length;
+                normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+                // Create a working TextRange that lives only in the input
+                textInputRange = el.createTextRange();
+                textInputRange.moveToBookmark(range.getBookmark());
+
+                // Check if the start and end of the selection are at the very end
+                // of the input, since moveStart/moveEnd doesn't return what we want
+                // in those cases
+                endRange = el.createTextRange();
+                endRange.collapse(false);
+
+                if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                    start = end = len;
+                } else {
+                    start = -textInputRange.moveStart("character", -len);
+                    start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                    if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                        end = len;
+                    } else {
+                        end = -textInputRange.moveEnd("character", -len);
+                        end += normalizedValue.slice(0, end).split("\n").length - 1;
+                    }
+                }
             }
         }
 
-        urls.forEach(url => {
-            insertAtCursor(this.editor, `\n![${url}](${url})\n`)
-        })
+        return {
+            start: start,
+            end: end
+        };
+    }
 
+    surround(pref, sufx) {
+        sufx = sufx || pref
+        var sel = this.getInputSelection(this.editor), val = this.editor.value;
+        if (sel.start == sel.end) return
+        this.editor.value = val.slice(0, sel.start) + pref + val.slice(sel.start, sel.end) + sufx + val.slice(sel.end);
+        this.showPreview()
+    }
+
+    insertAtCursor(myField, myValue) {
+        //IE support
+        if (document.selection) {
+            myField.focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+        }
+        //MOZILLA and others
+        else if (myField.selectionStart || myField.selectionStart == '0') {
+            var startPos = myField.selectionStart;
+            var endPos = myField.selectionEnd;
+            myField.value = myField.value.substring(0, startPos)
+                + myValue
+                + myField.value.substring(endPos, myField.value.length);
+        } else {
+            myField.value += myValue;
+        }
+    }
+
+    insertImgLinks(urls) {
+        urls.forEach(url => {
+            this.insertAtCursor(this.editor, `![${url}](${url})\n`)
+        })
+        this.showPreview()
     }
 
     getContent() {
@@ -177,10 +302,47 @@ class posts {
     showPreview() {
         showdown.setFlavor('github');
         var converter = new showdown.Converter()
-        let attach = "\n\n" + this.attachments.map(file => `<a href='${file}'><img src='/posts-plugin/attach.png'> ${file.replace(/^.*[\\\/]/, '')}</a>`).join('\n')
-        this.preview.innerHTML = this.getTitle(this.title.value) + converter.makeHtml(this.editor.value + attach);
+        this.preview.innerHTML = this.getTitle(this.title.value) + converter.makeHtml(this.editor.value);
         this.preview.querySelectorAll('pre code').forEach((block) => {
             hljs.highlightBlock(block);
         });
+    }
+
+    updateAttachmentView() {
+        let self = this
+        this.attachmentsView.innerHTML = '<b>Attachments</b><br/><br/>  '
+        self.attachmentsView.style.display = self.attachments.length > 0 ? 'block' : 'none'
+        render('attachments-view', {
+            ele: 'div',
+            children: this.attachments.map(file => {
+                return {
+                    ele: 'div',
+                    classList: 'attachment',
+                    children: [
+                        {
+                            ele: 'a',
+                            classList: 'link',
+                            attribs: { href: file },
+                            children: [
+                                { ele: 'img', attribs: { src: '/posts-plugin/attach.png' } },
+                                { ele: 'span', text: file.replace(/^.*[\\\/]/, '') }
+                            ]
+                        },
+                        {
+                            ele: 'span',
+                            classList: 'deletebtn',
+                            attribs: { innerHTML : '&#10006;'},
+                            evnts: {
+                                click: function () {
+                                    self.attachments.splice(self.attachments.indexOf(file), 1);
+                                    this.parentNode.remove()
+                                    self.attachmentsView.style.display = self.attachments.length > 0 ? 'block' : 'none'
+                                }
+                            }
+                        }
+                    ]
+                }
+            })
+        }, () => 0, this.attachmentsView)
     }
 }
