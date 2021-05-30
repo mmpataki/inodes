332b0f777bfceed817a55b0a3ad3acc543da475e
Madhusoodan P <akshayapataki123@gmail.com>
2021-05-30 21:01:22 +0530

    support for external plugin directory

--- src/main/java/inodes/controllers/KlassController.java
+++ src/main/java/inodes/controllers/KlassController.java
@@ -5,17 +5,34 @@ import inodes.service.api.Observable;
 import inodes.service.api.KlassService;
 import inodes.service.api.UnAuthorizedException;
 import inodes.util.SecurityUtil;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.File;
 import java.util.List;
 
 @RestController
 @CrossOrigin
-public class KlassController {
+@Log4j
+public class KlassController extends WebMvcConfigurerAdapter {
 
     @Autowired
     KlassService KS;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String plugDir = System.getenv("PLUGINS_DIR");
+        String arg[] = {"file:./src/main/resources/static"};
+        if (plugDir != null && !plugDir.isEmpty() && new File(plugDir).exists()) {
+            arg = new String[]{"file:./src/main/resources/static", "file:" + plugDir};
+        } else {
+            log.warn("PLUGINS_DIR environment variable is not set, user plugins won't be available");
+        }
+        registry.addResourceHandler("/p/**").addResourceLocations(arg);
+    }
 
     @GetMapping("/klass/{id}")
     public Klass get(@PathVariable String id) throws Exception {
